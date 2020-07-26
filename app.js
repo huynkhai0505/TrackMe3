2953abfd60fc741fd458dff9bed3edbd080fde68
huynkhai0505 <huynhkhai0505@gmail.com>
2020-07-26 16:12:52 +0700

    NEW: Add webpage for bio

---
+++ app.js
@@ -0,0 +1,24 @@
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.forEach(function(device) { 
+    $('#devices tbody').append(`
+    <tr>
+      <td>${device.user}</td> 
+      <td>${device.name}</td>
+    </tr>`
+  ); });
+
+  //Add device
+$('#add-device').on('click', function() { 
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user: user, name: name }); 
+    localStorage.setItem('devices', JSON.stringify(devices));
+    location.href = 'device-list.html';
+    });
+    
+    $('#send-command').on('click', function() { 
+        const command = $('#command').val(); 
+        console.log(`command is: ${command}`);
+});
+
